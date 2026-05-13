a4bdc8ad94eedf2e64f417fc65ee54df61b2e34b
dwrrio <107758775+dwrrio@users.noreply.github.com>
2026-05-14 00:35:16 +0330

    graph traversal test

--- tests/test_films.py
+++ tests/test_films.py
@@ -1,2 +1,19 @@
 from graphhop import *
 
+gh = GraphHop()
+
+gh.add_node("Inception", [0, 0, 0, 1])
+gh.add_node("Interstellar", [0, 0, 0, 1])
+gh.add_node("Dark Knight", [1, 0, 1, 0])
+gh.add_node("Superbad", [0, 1, 0, 0])
+gh.add_node("21 Jump Street", [0, 1, 0, 0])
+
+
+gh.add_edge("Inception", "Interstellar")
+gh.add_edge("Inception", "Dark Knight")
+gh.add_edge("Dark Knight", "Superbad")
+gh.add_edge("Superbad", "21 Jump Street")
+
+cue = gh.graph.nodes["Interstellar"]['feature']
+result = gh.navigate("Interstellar", cue, steps=2)
+print(result)
